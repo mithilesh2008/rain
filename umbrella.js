62363a6a5068209bd1cde2ebde7350886b8d67d8
mithilesh2008 <79626687+mithilesh2008@users.noreply.github.com>
2021-04-28 13:04:02 +0530

    Update umbrella.js

--- umbrella.js
+++ umbrella.js
@@ -5,7 +5,7 @@ class Umbrella {
         }
         this.body = Bodies.circle(x,y,50,options);
         this.radius = 50;
-        this.image = loadImage("images/Walking Frame/walking_1.png","images/Walking Frame/walking_2.png","images/Walking Frame/walking_3.png","images/Walking Frame/walking_4.png","images/Walking Frame/walking_5.png","images/Walking Frame/walking_6.png","images/Walking Frame/walking_7.png","images/Walking Frame/walking_8.png");
+        this.image = loadImage("images/Walking Frame/walking_1.png);
         World.add(world, this.body)
     }
 
